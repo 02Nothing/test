48fb0a012cdac6a1bfcf4a9bcd62745d0450ad94
Rostyslav Pashkevy <Rostyslav.Pashkevy@livenau.net>
2020-06-03 12:57:12 +0300

    Добавьте файлы проекта.

---
+++ tests/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        int res=Test.Calculate(15, 4);
+       
+        }
+    }
+static class Test
+    {
+        public static int Calculate(int a,int b)
+        {
+            return a * b;
+        }
+    }
+}
